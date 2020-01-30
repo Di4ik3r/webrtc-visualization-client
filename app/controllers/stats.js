c5b34ecdaab77adb10b6f72c7d8ccf6f241bce47
Tymofiev <ilia.tumofiev@gmail.com>
2020-01-30 12:49:12 +0200

    simple search outside of component

--- app/controllers/stats.js
+++ app/controllers/stats.js
@@ -4,6 +4,16 @@ import { action } from "@ember/object";
 export default class StatsController extends Controller {
   @action
   searchStat() {
-    console.log("clicked");
+    console.log(this.session_id);
+    this.store
+      .query("statistic", {
+        filter: {
+          lesson_id: this.session_id
+        }
+      })
+      .then(result => {
+        console.log(result);
+        this.model = result;
+      });
   }
 }
